725ae2fdb61adc6566328d109cc9afe972d5c45c
TWI Fan <twifan860@gmail.com>
2021-12-27 11:17:53 -0800

    Updates

---
+++ precache-manifest.50a56bb6fab9de6861a8104fc97b143e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5eba102f5b5e413df2b65c73f288afa",
+    "url": "/discord-react-clone/static/media/unknownGameIcon.a5eba102.svg"
+  },
+  {
+    "revision": "4d254296157bb8927b7d53ed59beb0d8",
+    "url": "/discord-react-clone/static/media/searchBarEyeGlass.4d254296.svg"
+  },
+  {
+    "revision": "368c9f2ca04e43b5a5ce7380d4622149",
+    "url": "/discord-react-clone/static/media/searchBarClear.368c9f2c.svg"
+  },
+  {
+    "revision": "4e012f05f9d0aaa1fc38b9836bcf8c69",
+    "url": "/discord-react-clone/static/media/close.4e012f05.svg"
+  },
+  {
+    "revision": "6049f8d7e5a27be3993c",
+    "url": "/discord-react-clone/static/js/runtime~main.6049f8d7.js"
+  },
+  {
+    "revision": "bb766fb9fc20ed0361d4",
+    "url": "/discord-react-clone/static/js/main.bb766fb9.chunk.js"
+  },
+  {
+    "revision": "69d99dab67597d136353",
+    "url": "/discord-react-clone/static/js/1.69d99dab.chunk.js"
+  },
+  {
+    "revision": "69d99dab67597d136353",
+    "url": "/discord-react-clone/static/css/1.03a46144.chunk.css"
+  },
+  {
+    "revision": "575bd7cd324b81f6a8b47614bf45d3e0",
+    "url": "/discord-react-clone/index.html"
+  }
+];
